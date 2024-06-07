ed32873a41c5b31fd54e5f670c31218179d69d13
Gustavo <gustadev@yahoo.com>
2024-06-07 16:22:10 -0300

    Criação textos json

--- Responsive Web Development/src/app/Inicial/page.tsx
+++ Responsive Web Development/src/app/Inicial/page.tsx
@@ -6,6 +6,7 @@ import Mar from '../../assets/mar.jpg'
 import Praia from '../../assets/mardois.jpg'
 import Grafico from '../../assets/Grafic.png'
 import Lixo from '../../assets/catarlixo.jpg'
+import Text from '../../data/Text.json'
 
 const Inicial = () => {
 
@@ -19,12 +20,8 @@ const Inicial = () => {
                         </div>
                             <div className={style.space_info}>
                                       <div className={style.text_inicial}>
-                                                <h2>Quem Somos</h2>
-                                                <p>A Ocean é uma plataforma inovadora dedicada a conectar cidadãos conscientes com empresas e 
-                                                organizações ambientais. Nosso objetivo é facilitar a denúncia e a resolução de problemas
-                                                relacionados à poluição dos oceanos, rios e à caça ilegal de animais marinhos. Queremos dar voz
-                                                àqueles que testemunham essas atividades prejudiciais, proporcionando uma maneira fácil e eficaz
-                                                de relatar essas ocorrências.</p>
+                                                <h2>{Text.sub_inicial_um}</h2>
+                                                <p>{Text.text_inicial_um}</p>
                                       </div>
                                       <div className={style.image_inicial}>
                                           <div className={style.img_wrapper}>
@@ -45,12 +42,8 @@ const Inicial = () => {
                                                 </div>
                                           </div>
                                           <div className={style.text_inicial}>
-                                                    <h2>Como Funciona</h2>
-                                                    <p>Nosso processo é simples: qualquer pessoa pode registrar na plataforma um reporte detalhado 
-                                                    sobre descarte incorreto de lixo, poluição ou caça ilegal, incluindo a fonte, descrição e localização do
-                                                    incidente. Esses reportes são armazenados em nosso painel, acessível por empresas e organizações 
-                                                    ambientais, que investigam e resolvem os problemas. A transparência permite acompanhar o status
-                                                    até a resolução final.</p>
+                                                    <h2>{Text.sub_inicial_dois}</h2>
+                                                    <p>{Text.text_inicial_dois}</p>
                                           </div>
                                 </div> 
                       </div>
@@ -61,9 +54,8 @@ const Inicial = () => {
                   <div className={style.box_normal}>
                             <div className={style.space_info}>
                                       <div className={style.text_inicial}>
-                                                <h2>Principais Lixos Encontrados nos Oceanos</h2>
-                                                <p>Os oceanos enfrentam uma variedade de desafios ambientais, e entre os mais preocupantes está o
-                                                acúmulo de lixo. Veja em nosso gráfico os principais resíduos encontrado nos ocenaos</p>
+                                                <h2>{Text.sub_inicial_tres}</h2>
+                                                <p>{Text.text_inicial_tres}</p>
                                       </div>
                                       <div className={style.image_inicial}>
                                               <Image className={style.img_dois} src={Grafico} alt='Mar'/>
@@ -82,11 +74,8 @@ const Inicial = () => {
                                              </div>
                                           </div>
                                           <div className={style.text_inicial}>
-                                                    <h2>Nosso Impacto</h2>
-                                                    <p>Cada reporte feito na nossa plataforma ajuda a criar um mapa detalhado das áreas mais afetadas,
-                                                    permitindo uma resposta rápida e eficaz das organizações competentes. Nossa missão é não apenas 
-                                                    mitigar os danos já causados, mas também promover uma cultura de responsabilidade ambiental, 
-                                                    incentivando todos a serem vigilantes e proativos na preservação dos oceanos.</p>        
+                                                    <h2>{Text.sub_inicial_quatro}</h2>
+                                                    <p>{Text.text_inicial_quatro}</p>        
                                           </div>
                                 </div> 
                       </div>
@@ -95,8 +84,7 @@ const Inicial = () => {
 
             <div className={style.container_inicial}>
                   <div className={style.final}>
-                          <p>Junte-se a nós na Ocean e faça parte da mudança. Seu reporte pode ser o primeiro passo para um oceano mais limpo e saudável 
-                            para as futuras gerações.</p>
+                          <p>{Text.text_inicial_cinco}</p>
                             <Link href='/Denuncia'><button className={style.button_inicial}>Novo Reporte</button></Link>
                   </div>
             </div>
