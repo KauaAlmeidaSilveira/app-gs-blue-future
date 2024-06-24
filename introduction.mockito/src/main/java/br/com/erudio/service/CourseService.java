d1af72389324be1189836da80c9f389cdb612a39
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-06-24 16:43:28 -0300

    remove introduction mockito

--- introduction.mockito/src/main/java/br/com/erudio/service/CourseService.java
+++
@@ -1,8 +0,0 @@
-package br.com.erudio.service;
-
-import java.util.List;
-
-public interface CourseService {
-    public List<String> retrieveCourses(String student);
-//    public List<String> doSomething(String student);
-}
